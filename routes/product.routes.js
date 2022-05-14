4a005b865d072fcd8cf4f6a40e5df862e131bf53
aBoswell <A.B0ZZW3LL@gmail.com>
2022-05-14 13:12:12 -0400

    product routes added - product removal, details, qty change

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,6 +5,19 @@ const Product = require("../models/Product.model");
 const Pantry = require("../models/Pantry.model")
 
 
+//***** RETREIVE PANTRY PRODUCT DETAILS *****//
+router.get('/:productId', (req, res, next) => {
+
+  const { productId } = req.params
+
+  Product.findById(productId)
+  .then(product => {
+    res.json(product)
+  })
+  .catch(error => console.log(error))
+})
+
+
 //***** HANDLE PRODUCT CREATION AND ADDING TO SPECIFIED PANTRY ******//
 router.post('/create', (req, res, next) => {
 
@@ -40,6 +53,38 @@ router.post('/create', (req, res, next) => {
       res.json('Product added to pantry');
     })
     .catch(error => console.log(error))
+})
+
+
+//***** HANDLE PRODUCT QUANTITY CHANGE ******//
+router.put('/change/:productId', (req, res, next) => {
+
+  const { productId } = req.params
+  const { productQty } = req.body
+
+  Product.findByIdAndUpdate(productId, { qty: productQty})
+  .then(updatedProduct => {
+    res.json(updatedProduct);
+  })
+  .catch(error => console.log(error))
+})
+
+
+//***** HANDLE PRODUCT REMOVAL ******//
+router.delete('/remove/:productId', (req, res, next) => {
+
+  const { productId } = req.params
+
+  Product.findByIdAndDelete(productId)
+    .then(productRemoved => {
+      return Pantry.findByIdAndUpdate(productRemoved.pantryId, { $pull: { products: productRemoved._id } })
+    })
+    .then(() => {
+      console.log('Product removed from pantry');
+      res.json('Product removed from pantry');
+    })
+    .catch(error => console.log(error))
+
 })
 
 
